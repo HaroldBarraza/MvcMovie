f31b53aedbd49dede38684bef69db9fbc471b6a3
HaroldBarraza <132396790+HaroldBarraza@users.noreply.github.com>
2025-09-16 16:50:49 -0500

    add

--- Models/SeeData.cs
+++ Models/SeeData.cs
@@ -50,7 +50,47 @@ public static class SeedData
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
                     Rating = "R", 
-                    Price = 3.99M
+                    Price = 3.90M
+                },
+                new Movie
+                {
+                    Title = "The Godfather",
+                    ReleaseDate = DateTime.Parse("1972-3-24"),
+                    Genre = "Crime, Drama",
+                    Rating = "R",
+                    Price = 4.99M
+                },
+                new Movie
+                {
+                    Title = "Inception",
+                    ReleaseDate = DateTime.Parse("2010-7-16"),
+                    Genre = "Action, Sci-Fi, Thriller",
+                    Rating = "PG-13",
+                    Price = 5.99M
+                },
+                new Movie
+                {
+                    Title = "Parasite",
+                    ReleaseDate = DateTime.Parse("2019-5-30"),
+                    Genre = "Drama, Thriller",
+                    Rating = "R",
+                    Price = 6.99M
+                },
+                new Movie
+                {
+                    Title = "Mad Max: Fury Road",
+                    ReleaseDate = DateTime.Parse("2015-5-15"),
+                    Genre = "Action, Adventure, Sci-Fi",
+                    Rating = "R",
+                    Price = 4.49M
+                },
+                new Movie
+                {
+                    Title = "La La Land",
+                    ReleaseDate = DateTime.Parse("2016-12-9"),
+                    Genre = "Comedy, Drama, Music",
+                    Rating = "PG-13",
+                    Price = 5.49M
                 }
             );
             context.SaveChanges();
--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd392dc3431b239a2dd63febd66119186ae476f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7fb0a3bbeeb240528cf9d79e3d8a67469b9407")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
